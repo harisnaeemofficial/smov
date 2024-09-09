648389bc779ce4b86cf9477757da0a115396560c
Haris Naeem <harrisnaeem991@gmail.com>
2024-09-09 23:41:28 +0300

    add deltaX

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -18,12 +18,9 @@ import {
   categories,
   tvCategories,
 } from "@/utils/discover";
-
 import { SubPageLayout } from "./layouts/SubPageLayout";
 import { Icon, Icons } from "../components/Icon";
 import { PageTitle } from "./parts/util/PageTitle";
-
-
 export function Discover() {
   const { t } = useTranslation();
   const [genres, setGenres] = useState<Genre[]>([]);
