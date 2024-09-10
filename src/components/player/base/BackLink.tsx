4770f898186cda9ddc962b2bac19813a129cc77b
Haris Naeem <harrisnaeem991@gmail.com>
2024-09-10 04:23:10 +0300

    Update BackLink.tsx

--- src/components/player/base/BackLink.tsx
+++ src/components/player/base/BackLink.tsx
@@ -1,10 +1,1 @@
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
 
-import { Icon, Icons } from "@/components/Icon";
-
-export function BackLink(props: { url: string }) {
-  const { t } = useTranslation();
-
-
-}
